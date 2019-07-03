e5562aa6b50c07568986dc8c1b9d79ec828ffd1c
Tyler Schaffer <tpschaffer@gmail.com>
2019-07-03 16:21:29 -0600

    Start valid placement for board class
    Co-authored-by: David Barriga-Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -7,6 +7,8 @@ require 'minitest/pride'
 class BoardTest < Minitest::Test
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_board_exists
@@ -16,7 +18,7 @@ class BoardTest < Minitest::Test
   def test_cells
     assert_equal 16, @board.cells.length
     assert_equal Hash, @board.cells.class
-    assert_equal Cell, @board.cells['A1'].class
+    assert_equal Cell, @board.cells["A1"].class
   end
 
   def test_if_cell_coordinates_are_valid
@@ -26,5 +28,16 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.valid_coordinate?("E1")
     assert_equal false, @board.valid_coordinate?("A22")
   end
+
+  def test_placement_for_length
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+  end
+
+  def test_placement_is_consecutive
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
+  end
+
 
 end
